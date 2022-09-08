89731b00a0034cee979a9f6cf92e0e588782dee9
tigistzelalem <makgg015@gmail.com>
2022-09-08 07:12:31 -0700

    files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
 int main(void)
-{puts("\"programming is like building a multilingual puzzle");
-return(0);
+{
+puts("\"programming is like building a multilingual puzzle");
+retur(0);
 }
+
