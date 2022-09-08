91f394f13072aa56704f8210ff2d8c599e192d7c
tigistzelalem <makgg015@gmail.com>
2022-09-08 06:59:13 -0700

    files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+*main entry point
+*
+*return  always 0
+*/
 int main(void)
-{printf("with proper grammer, but the oucome is a piece of art,");
+printf("with proper grammer, but the outcome is a piece of art,\n");
 return(0);
 }
