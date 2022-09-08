232a179717c31c7cdd13d9e3868fbe3376b7306d
tigistzelalem <makgg015@gmail.com>
2022-09-08 06:08:40 -0700

    c files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio.h>
+int main(void)
+{
+puts("\"programming is likebuilding a multilingual puzzle");
+return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammer, but the outcome isa piece of art,\n");
+return(0);
+}
