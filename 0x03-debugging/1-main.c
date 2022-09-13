c7204f660b331932c5148641bb8a90b4837b7e20
tigistzelalem <makgg015@gmail.com>
2022-09-13 00:51:50 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+int i;
+printf)"Infnite loop coming : (\n");
+i = 0;
+/*while (i<10)*/
+/*{*/
+/*_putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
+
