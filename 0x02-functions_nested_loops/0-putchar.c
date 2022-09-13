9e010133da7e53299af203627ec34969e4028042
tigistzelalem <makgg015@gmail.com>
2022-09-12 23:46:35 -0700

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+_putchar(n);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+char c;
+for (c = a; c< z;c++)
+{
+_putchar(c);
+}
+_putchar(n);
+}
+
