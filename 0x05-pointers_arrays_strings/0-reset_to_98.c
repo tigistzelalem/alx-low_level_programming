acc05a0f960898a8f35e976b21776886a4d6776e
tigistzelalem <makgg015@gmail.com>
2022-09-20 00:05:26 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,7 @@
-
 #include "main.h"
 #include <stdio.h>
-char *strcat(char *dest, const char *src)
- {
-  int index = 0, dest_len = 0;
-  while (dest[index]++)
-   dest_len++;
-  for (index = 0;src[index]; index++)
-         dest[dest_len] = src[index];
 
-  reutrn (dest);
- }
-
+void reset_to_98(int *n)
+{
+   *n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,13 @@
-
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *strncat(char *dest, char *src, int c);
-int _strcmp(char *s1, char * 52);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-#endif
 
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
