fb5021d89d24deee90b9e4465605689af070aba8
tigistzelalem <makgg015@gmail.com>
2022-09-20 03:21:49 -0700

    swap int

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_strncat(char *dest, , char *src, int n)
+/**
+*swap_int swap int values
+*@a first integer
+*@b second integer
+* return nothing
+*/
+
+void swap_int(int *a, int *b)
 {
-int index = 0, dest_len = 0;
-while (dest[[index]++)
-dest_len++;
-for (index = 0; src[index] && index < n ; index++)
-dest[dest_len++] = src[[index];
-return (dest);
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
-' > 1-swap.c
